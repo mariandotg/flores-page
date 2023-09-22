6bf62de7fba28247b7c85cc75b86dee6561c39e1
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-09-22 15:25:42 -0300

    Added new main js file

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const startBtn = document.getElementById('btn-start');
+const resetBtn = document.getElementById('btn-reset');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+startBtn.addEventListener('click', () => {
+  console.log("click")
+  var animatedElements = document.querySelectorAll('.hidden');
+  animatedElements.forEach(function (element) {
+    element.classList.remove('hidden')
+    element.classList.add('animated')
+    element.addEventListener('animationstart', function () {
+      setTimeout(() => element.classList.remove('fill-transp'), 1800)
+    });
+  });
+})
 
-setupCounter(document.querySelector('#counter'))
+resetBtn.addEventListener('click', () => {
+  var animatedElements = document.querySelectorAll('.animated');
+  animatedElements.forEach(function (element) {
+    element.classList.remove('animated')
+    element.classList.add('hidden')
+    element.classList.add('fill-transp')
+  });
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+  var animatedElements = document.querySelectorAll('.animated');
+  animatedElements.forEach(function (element) {
+    element.addEventListener('animationstart', function () {
+      setTimeout(() => element.classList.remove('fill-transp'), 1800)
+    });
+  });
+});
